09297199f85a96d28f46d7b162d2b6f50c6b2fcd
alfredsprojects <anemail4work@protonmail.com>
2021-01-28 19:13:02 -0800

    checking async burger functions

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "I am awesome"}
+
+
+async def get_burgers(number: int):
+    # Do some asynchronous stuff to create the burgers
+    burgers = number
+    return burgers
+
+@app.get('/burgers')
+async def read_burgers():
+    burgers = await get_burgers(2)
+    
+    return burgers
